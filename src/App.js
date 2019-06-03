460cba351a7de8a214dc3d7a5c63ea20763d91f3
Huynh Dang Khoa <hdkhoa1302@gmail.com>
2019-06-03 15:15:46 +0700

    update vie webstorm test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST 12345
+          TEST 1234
         </p>
         <a
           className="App-link"
